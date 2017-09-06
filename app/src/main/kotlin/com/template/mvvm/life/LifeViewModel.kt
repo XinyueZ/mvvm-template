5ff387040e0e2e163f7246839e5c35baabb0f2a8
Chris Zhao <xinyue.zhao@immonet.de>
2017-09-06 11:16:17 +0200

    Removed register in LifeViewModel

--- app/src/main/kotlin/com/template/mvvm/life/LifeViewModel.kt
+++ app/src/main/kotlin/com/template/mvvm/life/LifeViewModel.kt
@@ -2,12 +2,6 @@ package com.template.mvvm.life
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.LifecycleRegistry
-import android.arch.lifecycle.LifecycleRegistryOwner
 
-abstract class LifeViewModel(context: Application) : AndroidViewModel(context), LifecycleRegistryOwner {
-
-    private val registry = LifecycleRegistry(this)
-
-    override fun getLifecycle(): LifecycleRegistry = registry
+abstract class LifeViewModel(context: Application) : AndroidViewModel(context)
 }
