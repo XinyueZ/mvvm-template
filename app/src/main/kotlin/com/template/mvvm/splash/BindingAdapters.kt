4b56f358949df7cefca9d2f8543ed5fb16b948d2
Chris Zhao <xinyue.zhao@immonet.de>
2017-09-07 13:25:47 +0200

    Splash now fullscreen

--- app/src/main/kotlin/com/template/mvvm/splash/BindingAdapters.kt
+++ app/src/main/kotlin/com/template/mvvm/splash/BindingAdapters.kt
@@ -2,11 +2,11 @@ package com.template.mvvm.splash
 
 import android.app.Activity
 import android.databinding.BindingAdapter
-import android.widget.FrameLayout
+import android.widget.ImageView
 import com.template.mvvm.home.HomeActivity
 
 @BindingAdapter("startHome")
-fun startHome(view: FrameLayout, startHome: Boolean) {
+fun startHome(view: ImageView, startHome: Boolean) {
     when (startHome) {
         true -> {
             val cxt = view.context as Activity
--- app/src/main/kotlin/com/template/mvvm/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/template/mvvm/splash/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.template.mvvm.splash
 
 import android.arch.lifecycle.LifecycleFragment
+import android.os.Bundle
 import com.template.mvvm.R
 import com.template.mvvm.life.LifeActivity
+import com.template.mvvm.utils.SystemUiHelper
 
 class SplashActivity : LifeActivity() {
 
@@ -10,4 +12,17 @@ class SplashActivity : LifeActivity() {
 
     override fun obtainViewModelView() = (supportFragmentManager.findFragmentById(R.id.contentFrame) ?:
             SplashFragment.newInstance(application)) as LifecycleFragment
+
+    private lateinit var uiHelper: SystemUiHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        uiHelper = SystemUiHelper(this, SystemUiHelper.LEVEL_IMMERSIVE, 0)
+        uiHelper.delayHide(1000)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        uiHelper.hide()
+        super.onWindowFocusChanged(hasFocus)
+    }
 }
