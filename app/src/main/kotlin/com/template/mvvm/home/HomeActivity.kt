fa9363e719c27042d6670e4c811ac93e1a11a829
Chris Zhao <xinyue.zhao@immonet.de>
2017-09-07 13:29:46 +0200

    Added abstract getLayout in life-activity

--- app/src/main/kotlin/com/template/mvvm/home/HomeActivity.kt
+++ app/src/main/kotlin/com/template/mvvm/home/HomeActivity.kt
@@ -5,6 +5,7 @@ import com.template.mvvm.R
 import com.template.mvvm.life.LifeActivity
 
 class HomeActivity : LifeActivity() {
+    override fun getLayout() = R.layout.activity_home
     override fun createViewModel() = HomeViewModel::class.java
     override fun obtainViewModelView() = (supportFragmentManager.findFragmentById(R.id.contentFrame) ?: HomeFragment.newInstance(application)) as LifecycleFragment
 }
