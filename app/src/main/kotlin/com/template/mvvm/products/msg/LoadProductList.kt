e0888a1881e5127cb492a6091f6ed12fbee8bf17
Chris Zhao <xinyue.zhao@immonet.de>
2017-09-14 22:38:53 +0200

    Added Debug-Tool: LL for logcat

--- app/src/main/kotlin/com/template/mvvm/products/msg/LoadProductList.kt
+++
@@ -1,8 +0,0 @@
-package com.template.mvvm.products.msg
-
-import com.template.mvvm.actor.Detail
-import com.template.mvvm.actor.Message
-
-class LoadProductList(private val msg: String) : Message<Detail<String>> {
-    override fun getDetail() = Detail(msg)
-}
