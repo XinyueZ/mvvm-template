ad5ccd67cbef69564d83a8e6cda09d5e7c324cc3
Chris Zhao <xinyue.zhao@immonet.de>
2017-09-17 01:44:16 +0200

    Fixed creation of retrofit for service

--- app/src/main/kotlin/com/template/mvvm/data/repository/remote/ApiService.kt
+++ app/src/main/kotlin/com/template/mvvm/data/repository/remote/ApiService.kt
@@ -3,6 +3,8 @@ package com.template.mvvm.data.repository.remote
 import com.template.mvvm.data.feeds.products.ProductsData
 import io.reactivex.Single
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface ProductsApi {
@@ -11,6 +13,7 @@ interface ProductsApi {
 
     companion object {
         val service =
-                Retrofit.Builder().baseUrl("https://api.zalando.com/").build().create(ProductsApi::class.java)
+                Retrofit.Builder().baseUrl("https://api.zalando.com/").addConverterFactory(GsonConverterFactory.create())
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build().create(ProductsApi::class.java)
     }
 }
