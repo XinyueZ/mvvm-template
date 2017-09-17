f719abd0c2d84bfca9bfdb742f0ba65c0a166db6
Chris Zhao <xinyue.zhao@immonet.de>
2017-09-17 17:35:06 +0200

    Used zip to remote

--- app/src/main/kotlin/com/template/mvvm/data/repository/remote/LicensesRemote.kt
+++ app/src/main/kotlin/com/template/mvvm/data/repository/remote/LicensesRemote.kt
@@ -6,22 +6,22 @@ import com.template.mvvm.data.domain.licenses.LibraryList
 import com.template.mvvm.data.repository.LicensesDataSource
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Consumer
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 
 class LicensesRemote : LicensesDataSource {
     private val libraryList = LibraryList()
     override fun getAllLibraries(lifecycleOwner: LifecycleRegistryOwner): Single<LibraryList> {
-        return Single.just(libraryList).doFinally {
-            LicensesApi.service.getLibraries().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(Consumer {
-                libraryList.value = arrayListOf<Library>().apply {
-                    it.licenses.forEach({ licenseData ->
+        return Single.zip(Single.just(libraryList), LicensesApi.service.getLibraries().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()), BiFunction({ p1, p2 ->
+            libraryList.apply {
+                value = arrayListOf<Library>().apply {
+                    p2.licenses.forEach({ licenseData ->
                         licenseData.libraries.forEach({ libraryData ->
-                            this@apply.add(Library.from(libraryData, licenseData))
+                            add(Library.from(libraryData, licenseData))
                         })
                     })
                 }
-            })
-        }
+            }
+        }))
     }
 }
--- app/src/main/kotlin/com/template/mvvm/data/repository/remote/ProductsRemote.kt
+++ app/src/main/kotlin/com/template/mvvm/data/repository/remote/ProductsRemote.kt
@@ -7,25 +7,24 @@ import com.template.mvvm.data.domain.products.ProductList
 import com.template.mvvm.data.repository.ProductsDataSource
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Consumer
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 
 class ProductsRemote : ProductsDataSource {
     private val productList = ProductList()
     override fun getAllProducts(lifecycleOwner: LifecycleRegistryOwner): Single<ProductList> {
-        return Single.just(productList).doFinally {
-            ProductsApi.service.getArticles().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(Consumer {
-                arrayListOf<Product>().apply {
-                    it.products.forEach {
+        return Single.zip(Single.just(productList), ProductsApi.service.getArticles().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()), BiFunction({ p1, p2 ->
+            p1.apply {
+                value = (arrayListOf<Product>()).apply {
+                    p2.products.forEach {
                         add(Product(
                                 it.name,
                                 String.format("%s//%s//%s", it.brand.name, it.genders.joinToString(), it.ageGroups.joinToString()),
                                 Uri.parse(it.media.images.first().largeHdUrl),
                                 if (it.brand.logo != null) Uri.parse(it.brand.logo) else Uri.EMPTY))
                     }
-                    productList.value = this
                 }
-            })
-        }
+            }
+        }))
     }
 }
