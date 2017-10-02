aded98c977739446bd70f92502ece21c79c21a7e
Chris Zhao <xinyue.zhao@immonet.de>
2017-10-02 14:44:10 +0200

    Added default and error image on photo

--- core/src/main/kotlin/com/template/mvvm/binding/BindingAdapters.kt
+++ core/src/main/kotlin/com/template/mvvm/binding/BindingAdapters.kt
@@ -4,16 +4,25 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.arch.lifecycle.ViewModel
 import android.databinding.BindingAdapter
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.support.annotation.DrawableRes
 import android.support.design.internal.BottomNavigationItemView
 import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.NavigationView
 import android.support.v4.app.ActivityCompat
 import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.content.res.AppCompatResources
 import android.support.v7.widget.Toolbar
 import android.view.View
 import android.widget.ImageView
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.template.mvvm.GlideApp
 import com.template.mvvm.LL
 import com.template.mvvm.R
@@ -24,12 +33,10 @@ fun a(view: View, width: Int, height: Int) {
     view.layoutParams.height = height
 }
 
-
 @BindingAdapter("stopLoading")
 fun b(view: SwipeRefreshLayout, stopLoading: Boolean) {
     view.isRefreshing = !stopLoading
 }
-
 
 @BindingAdapter("reload")
 fun c(view: SwipeRefreshLayout, l: OnReloadListener) {
@@ -38,12 +45,76 @@ fun c(view: SwipeRefreshLayout, l: OnReloadListener) {
     }
 }
 
-@BindingAdapter("remoteImageUri")
-fun d(view: View, uri: Uri?) {
+@BindingAdapter(value = *arrayOf("remoteImageUri", "placeholderRes", "errorDrawableRes"), requireAll = false)
+fun d(view: View, uri: Uri?, @DrawableRes placeholderRes: Int, @DrawableRes errorDrawableRes: Int) {
     uri?.let {
-        if (view is ImageView)
-            GlideApp.with(view).load(uri).into(view)
+        if (view is ImageView) {
+            d(view, arrayOf(uri),
+                    placeholderRes,
+                    errorDrawableRes)
+        }
     }
+}
+
+@BindingAdapter(value = *arrayOf("remoteImageUris", "placeholderRes", "errorDrawableRes"), requireAll = false)
+fun d(view: ImageView, imageUris: Array<Uri>?, @DrawableRes placeholderRes: Int, @DrawableRes errorDrawableRes: Int) {
+    when (imageUris == null || imageUris.isEmpty()) {
+        true -> {
+            when (errorDrawableRes > 0) {
+                true -> view.setImageResource(errorDrawableRes)
+                false -> view.setImageBitmap(null)
+            }
+            return
+        }
+    }
+
+    imageUris?.let {
+        val error: Drawable? = when (errorDrawableRes <= 0) {
+            true -> null
+            else -> AppCompatResources.getDrawable(view.context, errorDrawableRes)
+        }
+
+        val placeholder: Drawable? = when (placeholderRes <= 0) {
+            true -> null
+            else -> AppCompatResources.getDrawable(view.context, placeholderRes)
+        }
+
+        var pos = 0
+        val listener = object : RequestListener<Bitmap> {
+            override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: com.bumptech.glide.load.DataSource?, isFirstResource: Boolean): Boolean {
+                // let glide set the image by returning false
+                return false
+            }
+
+            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+                // load next one if possible
+                when (pos < (imageUris.size - 1)) {
+                    true -> {
+                        pos++
+                        loadImage(view, imageUris[pos], placeholder, error, this)
+                        return true
+                    }
+                }
+                return false
+            }
+        }
+
+        loadImage(view, imageUris[pos], placeholder, error, listener)
+    }
+}
+
+private fun loadImage(view: ImageView, uri: Uri, placeholder: Drawable?, errorDrawable: Drawable?, listener: RequestListener<Bitmap>) {
+    GlideApp.with(view)
+            .asBitmap()
+            .load(uri)
+            .format(DecodeFormat.PREFER_RGB_565)
+            .placeholder(placeholder)
+            .error(errorDrawable)
+            .dontAnimate()
+            .skipMemoryCache(false)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .listener(listener)
+            .into(view)
 }
 
 @BindingAdapter("goBack")
@@ -92,8 +163,9 @@ fun j(view: View, l: OnItemCommandListener, vm: ViewModel) {
     }
 }
 
-// Some view-ext
-
+//
+// Some view-ext, helpers
+//
 @SuppressLint("RestrictedApi")
 fun BottomNavigationView.disableShiftMode() {
     val menuView = this.getChildAt(0) as BottomNavigationMenuView
