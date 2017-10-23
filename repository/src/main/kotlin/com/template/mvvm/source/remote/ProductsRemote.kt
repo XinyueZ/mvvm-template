b170a31c5f2bfb1d1ec422341b758547fd8bed2f
Chris Zhao <xinyue.zhao@immonet.de>
2017-10-23 21:39:13 +0200

    Optimized product remote call using map

--- repository/src/main/kotlin/com/template/mvvm/source/remote/ProductsRemote.kt
+++ repository/src/main/kotlin/com/template/mvvm/source/remote/ProductsRemote.kt
@@ -9,51 +9,42 @@ import kotlinx.coroutines.experimental.channels.produce
 
 class ProductsRemote : ProductsDataSource {
 
-    override suspend fun getAllProducts(job: Job, localOnly: Boolean) = produce<List<Product>>(job) {
+    override suspend fun getAllProducts(job: Job, localOnly: Boolean) = produce(job) {
         ProductsApi.service.getArticles().execute().takeIf {
             it.isSuccessful
         }?.let {
             it.body()?.let {
-                mutableListOf<Product>().apply {
-                    it.products.forEach {
-                        add(Product.from(it))
-                    }
-                    LL.d("products loaded from net")
-                    send(this)
-                }
+                LL.d("products loaded from net")
+                send(it.products.map {
+                    Product.from(it)
+                })
             }
         }
     }
 
-    override suspend fun filterProduct(job: Job, keyword: String, localOnly: Boolean) = produce<List<Product>>(job) {
+    override suspend fun filterProduct(job: Job, keyword: String, localOnly: Boolean) = produce(job) {
         ProductsApi.service.filterArticles(keyword).execute().takeIf {
             it.isSuccessful
         }?.let {
             it.body()?.let {
-                mutableListOf<Product>().apply {
-                    it.products.forEach {
-                        add(Product.from(it))
-                    }
-                    LL.d("filtered $keyword products and loaded from net")
-                    send(this)
-                }
+                LL.d("filtered $keyword products and loaded from net")
+                send(it.products.map {
+                    Product.from(it)
+                })
             }
         }
     }
 
-    override suspend fun getAllBrands(job: Job, localOnly: Boolean) = produce<List<Brand>>(job) {
+    override suspend fun getAllBrands(job: Job, localOnly: Boolean) = produce(job) {
         ProductsApi.service
                 .getBrands().execute().takeIf {
             it.isSuccessful
         }?.let {
             it.body()?.let {
-                mutableListOf<Brand>().apply {
-                    it.brands.forEach {
-                        add(Brand.from(it))
-                    }
-                    LL.d("brands loaded from net")
-                    send(this)
-                }
+                LL.d("brands loaded from net")
+                send(it.brands.map {
+                    Brand.from(it)
+                })
             }
         }
     }
