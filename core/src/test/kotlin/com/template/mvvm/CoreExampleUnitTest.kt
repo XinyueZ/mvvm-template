3f938e4bfd3a27544c1f316e9c09b0dd828369dc
Chris Zhao <xinyue.zhao@immonet.de>
2017-11-17 22:44:19 +0100

    Added powermock

--- core/src/test/kotlin/com/template/mvvm/CoreExampleUnitTest.kt
+++ core/src/test/kotlin/com/template/mvvm/CoreExampleUnitTest.kt
@@ -1,7 +1,17 @@
 package com.template.mvvm
 
-import org.junit.Assert.assertEquals
+import android.app.Application
+import android.content.Context
+import android.content.res.Resources
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import com.template.mvvm.source.Repository
+import junit.framework.Assert
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +19,30 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class CoreExampleUnitTest {
+    @Mock private lateinit var repo: Repository
+    @Mock private lateinit var context: Application
+
+    private fun setupContext() {
+        Mockito.`when`<Context>(context.applicationContext).thenReturn(context)
+        Mockito.`when`(context.resources).thenReturn(Mockito.mock(Resources::class.java))
+
+        val connectivityManager = Mockito.mock(ConnectivityManager::class.java)
+        val networkInfo = Mockito.mock(NetworkInfo::class.java)
+        Mockito.`when`(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)).thenReturn(networkInfo)
+        Mockito.`when`(networkInfo.isAvailable).thenReturn(true)
+        Mockito.`when`(networkInfo.isConnected).thenReturn(true)
+
+    }
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        setupContext()
+        repo = Injection.getInstance(context).provideRepository(context)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testGetLicense() {
+        Assert.assertNotNull(repo)
     }
 }
