12062148d4f96372e3ff85150690ed2f67e03064
Chris Zhao <xinyue.zhao@immonet.de>
2017-12-06 23:33:31 +0100

    Fixed ViewModelFactory for new repository-injection location

--- repository/src/testMock/kotlin/com/template/mvvm/source/TestRepository.kt
+++ repository/src/testMock/kotlin/com/template/mvvm/source/TestRepository.kt
@@ -2,6 +2,8 @@ package com.template.mvvm.source
 
 import com.template.mvvm.RepositoryInjection
 import com.template.mvvm.RepositoryTestRule
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,11 +14,10 @@ class TestRepository {
     @Rule
     fun test() = RepositoryTestRule()
 
-
     @Test
     fun testSingleton() {
         val ins_1 = RepositoryInjection.getInstance()
         val ins_2 = RepositoryInjection.getInstance()
-        assert(ins_1 == ins_2)
+        assertThat(ins_1, CoreMatchers.`equalTo`(ins_2))
     }
 }
