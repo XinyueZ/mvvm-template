4386bac7e753c6f8898313c6921f24d0deb03081
Chris Zhao <xinyue.zhao@immonet.de>
2017-12-08 10:26:29 +0100

    Changed mock net-behavie

--- repository/src/mock/kotlin/com/template/mvvm/source/remote/NetworkInjection.kt
+++ repository/src/mock/kotlin/com/template/mvvm/source/remote/NetworkInjection.kt
@@ -3,13 +3,21 @@ package com.template.mvvm.source.remote
 import android.content.Context
 import retrofit2.Retrofit
 import retrofit2.mock.MockRetrofit
+import retrofit2.mock.NetworkBehavior
 
 internal object NetworkInjection {
+    private val behavior = NetworkBehavior.create().apply {
+        setErrorPercent(0)
+        setFailurePercent(0)
+        setVariancePercent(0)
+    }
+
     fun provideProductsApiService(context: Context, retrofitBuilder: Retrofit.Builder) =
             MockProductsApi(context, MockRetrofit.Builder(
                     retrofitBuilder
                             .baseUrl("http://www.hello.com")
                             .build())
+                    .networkBehavior(behavior)
                     .build()
                     .create(ProductsApi::class.java))
 
@@ -18,6 +26,7 @@ internal object NetworkInjection {
                     retrofitBuilder
                             .baseUrl("http://www.hello.com")
                             .build())
+                    .networkBehavior(behavior)
                     .build()
                     .create(LicensesApi::class.java))
 }
