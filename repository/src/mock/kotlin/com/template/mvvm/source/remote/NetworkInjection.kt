ebfad9849f0c7fb51d11493fdb5d356e077f6654
Chris Zhao <xinyue.zhao@immonet.de>
2017-12-15 14:10:03 +0100

    Format NetworkInjection.kt

--- repository/src/mock/kotlin/com/template/mvvm/source/remote/NetworkInjection.kt
+++ repository/src/mock/kotlin/com/template/mvvm/source/remote/NetworkInjection.kt
@@ -13,20 +13,26 @@ internal object NetworkInjection {
     }
 
     fun provideProductsApiService(context: Context, retrofitBuilder: Retrofit.Builder) =
-            MockProductsApi(context, MockRetrofit.Builder(
-                    retrofitBuilder
-                            .baseUrl("http://www.hello.com")
-                            .build())
-                    .networkBehavior(behavior)
-                    .build()
-                    .create(ProductsApi::class.java))
+            MockProductsApi(
+                    context,
+                    MockRetrofit.Builder(
+                            retrofitBuilder
+                                    .baseUrl("http://www.hello.com")
+                                    .build())
+                            .networkBehavior(behavior)
+                            .build()
+                            .create(ProductsApi::class.java)
+            )
 
     fun provideLicensesApiService(context: Context, retrofitBuilder: Retrofit.Builder) =
-            MockLicensesApi(context, MockRetrofit.Builder(
-                    retrofitBuilder
-                            .baseUrl("http://www.hello.com")
-                            .build())
-                    .networkBehavior(behavior)
-                    .build()
-                    .create(LicensesApi::class.java))
+            MockLicensesApi(
+                    context,
+                    MockRetrofit.Builder(
+                            retrofitBuilder
+                                    .baseUrl("http://www.hello.com")
+                                    .build())
+                            .networkBehavior(behavior)
+                            .build()
+                            .create(LicensesApi::class.java)
+            )
 }
