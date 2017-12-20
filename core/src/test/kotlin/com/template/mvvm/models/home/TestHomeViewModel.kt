57c07161d905e403e01c13ee0c4199d99a3ba104
Chris Zhao <xinyue.zhao@immonet.de>
2017-12-20 22:08:19 +0100

    Added tests for init of HomeViewModel

--- core/src/test/kotlin/com/template/mvvm/models/home/TestHomeViewModel.kt
+++ core/src/test/kotlin/com/template/mvvm/models/home/TestHomeViewModel.kt
@@ -1,8 +1,25 @@
 package com.template.mvvm.models.home
 
+import com.template.mvvm.R
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class TestHomeViewModel {
+    private lateinit var homeVm: HomeViewModel
+
+    @Before
+    fun setup() {
+        homeVm = HomeViewModel()
+    }
+
+    @Test
+    fun testHomeModelInit() {
+        // Test state init
+        assertThat(homeVm.state.title.get(), `equalTo`(R.string.home_title))
+    }
 }
