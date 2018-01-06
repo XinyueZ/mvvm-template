a9dd3799a90aee0065c85d75dcb541d4350c1566
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-06 22:36:45 +0100

    Updated TestAppUtils

--- app/src/test/kotlin/com/template/mvvm/app/TestAppUtils.kt
+++ app/src/test/kotlin/com/template/mvvm/app/TestAppUtils.kt
@@ -1,10 +1,30 @@
 package com.template.mvvm.app
 
+import android.app.Activity
 import android.content.Context
+import android.support.annotation.IdRes
+import android.view.View
+import org.junit.Assert
+import org.robolectric.android.controller.ActivityController
 import org.robolectric.shadows.ShadowApplication
 import java.util.concurrent.TimeUnit
 
 fun context(): Context = ShadowApplication.getInstance().applicationContext
+fun <T : Activity> ActivityController<T>.finish() {
+    try {
+        pause().stop().destroy()
+    } catch (ignored: Throwable) {
+    }
+}
+
+fun <T : View> ActivityController<out Activity>.applyView(@IdRes id: Int, block: T.() -> Unit = {}): T
+        = get().applyView(id, block)
+
+fun <T : View> Activity.applyView(@IdRes id: Int, block: T.() -> Unit = {}): T =
+        findViewById<T>(id).apply {
+            Assert.assertNotNull(this)
+            block(this)
+        }
 
 inline fun sleepWhile(interval: Long = 1000L, maxIntervals: Int = 10, crossinline condition: (() -> Boolean)) {
     var loop = 0
