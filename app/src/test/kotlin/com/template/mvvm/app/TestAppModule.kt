f3f0bc5aa7ad5ba41929d786a725be0ae8128779
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-06 01:01:25 +0100

    Added tests on App

---
+++ app/src/test/kotlin/com/template/mvvm/app/TestAppModule.kt
@@ -0,0 +1,27 @@
+package com.template.mvvm.app
+
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.notNullValue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class TestAppModule {
+    private var app: App? = null
+    @Rule
+    fun test() = AppTestRule()
+
+    @Before
+    fun setup() {
+        app = context() as? App
+    }
+
+    @Test
+    fun testAppExist() {
+        MatcherAssert.assertThat(app, `is`(notNullValue()))
+    }
+}
---
+++ app/src/test/kotlin/com/template/mvvm/app/TestAppUtils.kt
@@ -0,0 +1,25 @@
+package com.template.mvvm.app
+
+import android.content.Context
+import org.robolectric.shadows.ShadowApplication
+import java.util.concurrent.TimeUnit
+
+fun context(): Context = ShadowApplication.getInstance().applicationContext
+
+inline fun sleepWhile(interval: Long = 1000L, maxIntervals: Int = 10, crossinline condition: (() -> Boolean)) {
+    var loop = 0
+
+    while (condition()) {
+        TimeUnit.MILLISECONDS.sleep(interval)
+
+        loop++
+
+        if (loop >= maxIntervals) {
+            throw IllegalStateException("Slept longer than ${loop * interval}ms. Abort.")
+        }
+    }
+}
+
+
+
+
