bab8f555402c3be8b476844d9bfc312c6fbd2b35
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-06 23:22:46 +0100

    Added tests for splash, however, there's still an [issue](https://github.com/XinyueZ/mvvm-template/issues/3)

---
+++ app/src/test/kotlin/com/template/mvvm/app/splash/UiTestSplash.kt
@@ -0,0 +1,53 @@
+package com.template.mvvm.app.splash
+
+import com.template.mvvm.app.AppTestRule
+import com.template.mvvm.app.finish
+import com.template.mvvm.app.home.HomeActivity
+import kotlinx.coroutines.experimental.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
+import org.robolectric.android.controller.ActivityController
+import org.robolectric.shadows.ShadowLooper.idleMainLooper
+import java.util.concurrent.TimeUnit
+import kotlin.system.measureTimeMillis
+
+@RunWith(RobolectricTestRunner::class)
+class UiTestSplash {
+    private lateinit var activityCtrl: ActivityController<SplashActivity>
+    private val activity: SplashActivity
+        get() = activityCtrl.get()
+
+    @Rule
+    fun test() = AppTestRule()
+
+    @Before
+    fun init() {
+        activityCtrl = Robolectric.buildActivity(SplashActivity::class.java).setup()
+    }
+
+    @After
+    fun tearDown() {
+        activityCtrl.finish()
+    }
+
+    @Test
+    fun testSplashComplete() {
+        runBlocking {
+            Shadows.shadowOf(activity).run {
+                measureTimeMillis {
+                    idleMainLooper(2000, TimeUnit.MILLISECONDS)
+                }.apply {
+                    assertThat(nextStartedActivity.component.className, `is`(HomeActivity::class.java.name))
+                }
+            }
+        }
+    }
+}
