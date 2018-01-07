a2cf69dc336d71d2becaae0c7f990d12d31f6dd9
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-07 16:07:27 +0100

    Added ui-test on about

---
+++ app/src/test/kotlin/com/template/mvvm/app/about/UiTestAbout.kt
@@ -0,0 +1,68 @@
+package com.template.mvvm.app.about
+
+import android.support.v7.widget.Toolbar
+import android.widget.TextView
+import com.template.mvvm.app.AppTestRule
+import com.template.mvvm.app.BuildConfig
+import com.template.mvvm.app.R
+import com.template.mvvm.app.applyView
+import com.template.mvvm.app.context
+import com.template.mvvm.app.finish
+import com.template.mvvm.core.R.string
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
+
+@RunWith(RobolectricTestRunner::class)
+class UiTestAbout {
+    private lateinit var activityCtrl: ActivityController<AboutActivity>
+    private val activity: AboutActivity
+        get() = activityCtrl.get()
+
+    @Rule
+    fun test() = AppTestRule()
+
+    @Before
+    fun init() {
+        activityCtrl = Robolectric.buildActivity(AboutActivity::class.java).setup()
+    }
+
+    @After
+    fun tearDown() {
+        activityCtrl.finish()
+    }
+
+    @Test
+    fun testAboutContent() {
+        context().run {
+            activity.applyView<Toolbar>(R.id.toolbar) {
+                assertThat(getString(string.about_title), `equalTo`(title))
+            }
+
+            activity.applyView<TextView>(R.id.versionTitle) {
+                assertThat(getString(string.about_version_title), `equalTo`(text))
+            }
+
+            activity.applyView<TextView>(R.id.versionContent) {
+                assertThat(getString(string.about_version_content, BuildConfig.VERSION_NAME), `equalTo`(text))
+
+            }
+
+            activity.applyView<TextView>(R.id.descriptionTitle) {
+                assertThat(getString(string.about_description_title), `equalTo`(text))
+            }
+
+            activity.applyView<TextView>(R.id.descriptionContent) {
+                assertThat(getString(string.about_description_content), `equalTo`(text))
+
+            }
+        }
+    }
+}
