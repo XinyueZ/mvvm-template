c48a9a4777b0efd8f458c226f625e1769b2d0101
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-08 15:58:42 +0100

    Added tests for init-select on Home

--- app/src/test/kotlin/com/template/mvvm/app/home/UiTestHome.kt
+++ app/src/test/kotlin/com/template/mvvm/app/home/UiTestHome.kt
@@ -1,10 +1,18 @@
 package com.template.mvvm.app.home
 
+import android.support.design.widget.BottomNavigationView
 import com.template.mvvm.app.AppTestRule
+import com.template.mvvm.app.R
+import com.template.mvvm.app.advanceToNextPostedRunnable
+import com.template.mvvm.app.applyView
 import com.template.mvvm.app.finish
+import com.template.mvvm.app.sleepWhile
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
@@ -21,11 +29,22 @@ class UiTestHome {
 
     @Before
     fun init() {
-        activityCtrl = Robolectric.buildActivity(HomeActivity::class.java).setup()
+        activityCtrl = Robolectric.buildActivity(HomeActivity::class.java).create()
     }
 
     @After
     fun tearDown() {
         activityCtrl.finish()
+    }
+
+    @Test
+    fun testSelectMenOnStart() {
+        activity.applyView<BottomNavigationView>(R.id.bottomNavi) {
+            sleepWhile {
+                advanceToNextPostedRunnable()
+                R.id.action_men != selectedItemId
+            }
+            assertThat(selectedItemId, `is`(R.id.action_men))
+        }
     }
 }
