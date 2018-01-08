cb9c21a9df1301323e701f3a9f84d8b77169caf1
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-08 16:04:31 +0100

    Updated TestAppUtils

--- app/src/test/kotlin/com/template/mvvm/app/TestAppUtils.kt
+++ app/src/test/kotlin/com/template/mvvm/app/TestAppUtils.kt
@@ -2,11 +2,15 @@ package com.template.mvvm.app
 
 import android.app.Activity
 import android.content.Context
+import android.os.Handler
 import android.support.annotation.IdRes
 import android.view.View
 import org.junit.Assert
+import org.robolectric.Shadows
 import org.robolectric.android.controller.ActivityController
 import org.robolectric.shadows.ShadowApplication
+import org.robolectric.shadows.ShadowLooper
+import org.robolectric.util.Scheduler
 import java.util.concurrent.TimeUnit
 
 fun context(): Context = ShadowApplication.getInstance().applicationContext
@@ -39,6 +43,15 @@ inline fun sleepWhile(interval: Long = 1000L, maxIntervals: Int = 10, crossinlin
         }
     }
 }
+
+fun advanceToNextPostedRunnable(scheduler: Scheduler = ShadowLooper.getShadowMainLooper().scheduler) {
+    scheduler.advanceToNextPostedRunnable()
+}
+
+fun advanceToNextPostedRunnable(handler: Handler) {
+    advanceToNextPostedRunnable(Shadows.shadowOf(handler.looper).scheduler)
+}
+
 
 
 
