82bfbdb3c0488e5c9870d9c79ca41b1a7ac9964e
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-09 15:28:45 +0100

    Fixed problem to test drawer opening

--- app/src/test/kotlin/com/template/mvvm/app/home/UiTestHome.kt
+++ app/src/test/kotlin/com/template/mvvm/app/home/UiTestHome.kt
@@ -4,14 +4,12 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
+import android.view.View
 import com.template.mvvm.app.AppTestRule
 import com.template.mvvm.app.R
 import com.template.mvvm.app.advanceToNextPostedRunnable
 import com.template.mvvm.app.applyView
 import com.template.mvvm.app.finish
-import com.template.mvvm.app.sleepWhile
-import com.template.mvvm.core.ext.obtainViewModel
-import com.template.mvvm.core.models.home.HomeViewModel
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.junit.After
@@ -34,7 +32,7 @@ class UiTestHome {
 
     @Before
     fun init() {
-        activityCtrl = Robolectric.buildActivity(HomeActivity::class.java).create()
+        activityCtrl = Robolectric.buildActivity(HomeActivity::class.java).setup()
     }
 
     @After
@@ -45,65 +43,21 @@ class UiTestHome {
     @Test
     fun testSelectMenOnStart() {
         activity.applyView<BottomNavigationView>(R.id.bottomNavi) {
-            sleepWhile {
-                advanceToNextPostedRunnable()
-                R.id.action_men != selectedItemId
-            }
             assertThat(selectedItemId, `is`(R.id.action_men))
         }
     }
 
-    /**
-     *  Actually I want to run following codes which do pure UI execution to open drawer with
-     *  clicking "burg button", however, there's no way to call "click" on the reflected
-     *  [Toolbar.mNavButtonView].
-     **/
     @Test
     fun testOpenCloseNavigationDrawer1() {
-//        println("[${Thread.currentThread().name} ]")
-//        activity.applyView<Toolbar>(R.id.toolbar) {
-//            println("[${Thread.currentThread().name} ]")
-//            val field = Toolbar::class.memberProperties.find { it.name == "mNavButtonView" }
-//            field?.let { nonNullField ->
-//                println("[${Thread.currentThread().name} ]")
-//                nonNullField.isAccessible = true
-//                val clickView = nonNullField.get(this) as ImageButton
-//                println(clickView)
-//                println(clickView.hashCode())
-//
-////                Assert.assertTrue(clickView.callOnClick())
-////                Assert.assertTrue(clickView.performClick())
-////                assertThat(clickView.callOnClick(), `is`(true))
-//            }
-//        }
-
-//        activityCtrl.resume()
-
-//        activity.applyView<DrawerLayout>(R.id.drawer) {
-//            println("[${Thread.currentThread().name} ]")
-//            assertThat(isDrawerOpen(GravityCompat.START), `is`(true))
-//        }
-    }
-
-    @Test
-    fun testOpenCloseNavigationDrawer2() {
-        activity.applyView<DrawerLayout>(R.id.drawer) {
-            openDrawer(GravityCompat.START)
-            assertThat(isDrawerOpen(GravityCompat.START), `is`(true))
-            closeDrawer(GravityCompat.START)
-            assertThat(isDrawerOpen(GravityCompat.START), `is`(false))
-        }
-    }
-
-    @Test
-    fun testOpenCloseNavigationDrawer3() {
         activity.applyView<Toolbar>(R.id.toolbar) {
-            activity.obtainViewModel(HomeViewModel::class.java).onCommand(R.id.action_app_bar_indicator)
+            navigationContentDescription = "burg-button is good"
+            val vs = arrayListOf<View>()
+            findViewsWithText(vs, navigationContentDescription, View.FIND_VIEWS_WITH_CONTENT_DESCRIPTION)
+            assertThat(vs[0].callOnClick(), `is`(true))
         }
-
-        activityCtrl.resume()
-
         activity.applyView<DrawerLayout>(R.id.drawer) {
+            advanceToNextPostedRunnable()
+            computeScroll()
             assertThat(isDrawerOpen(GravityCompat.START), `is`(true))
         }
     }
