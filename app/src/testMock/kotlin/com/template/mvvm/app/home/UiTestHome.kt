9631ebae166a47054edf3c7165c330c0460dfd8f
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-10 10:04:14 +0100

    Added tests for clicking of drawer-menu-items

--- app/src/testMock/kotlin/com/template/mvvm/app/home/UiTestHome.kt
+++ app/src/testMock/kotlin/com/template/mvvm/app/home/UiTestHome.kt
@@ -1,5 +1,6 @@
 package com.template.mvvm.app.home
 
+import android.content.Intent
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
@@ -8,16 +9,18 @@ import android.support.v7.widget.Toolbar
 import android.view.View
 import com.template.mvvm.app.AppTestRule
 import com.template.mvvm.app.R
+import com.template.mvvm.app.about.AboutActivity
 import com.template.mvvm.app.advanceToNextPostedRunnable
 import com.template.mvvm.app.applyView
 import com.template.mvvm.app.finish
+import com.template.mvvm.app.licenses.SoftwareLicensesActivity
+import com.template.mvvm.app.products.ProductsActivity
 import com.template.mvvm.base.ext.findChildFragment
 import com.template.mvvm.base.ext.findSubItem
-import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers
 import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.equalTo
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -25,6 +28,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
 import org.robolectric.android.controller.ActivityController
 
 @RunWith(RobolectricTestRunner::class)
@@ -73,13 +77,13 @@ class UiTestHome {
         activity.run {
             applyView<BottomNavigationView>(R.id.bottomNavi) {
                 selectedItemId = R.id.action_men
-                assertThat(findChildFragment(R.id.contentFrame, R.id.childContentFrame)?.javaClass.toString(), Matchers.equalTo(MenFragment::class.java.toString()))
+                assertThat(findChildFragment(R.id.contentFrame, R.id.childContentFrame)?.javaClass.toString(), equalTo(MenFragment::class.java.toString()))
 
                 selectedItemId = R.id.action_women
-                assertThat(findChildFragment(R.id.contentFrame, R.id.childContentFrame)?.javaClass.toString(), Matchers.equalTo(WomenFragment::class.java.toString()))
+                assertThat(findChildFragment(R.id.contentFrame, R.id.childContentFrame)?.javaClass.toString(), equalTo(WomenFragment::class.java.toString()))
 
                 selectedItemId = R.id.action_all_genders
-                assertThat(findChildFragment(R.id.contentFrame, R.id.childContentFrame)?.javaClass.toString(), Matchers.equalTo(AllGendersFragment::class.java.toString()))
+                assertThat(findChildFragment(R.id.contentFrame, R.id.childContentFrame)?.javaClass.toString(), equalTo(AllGendersFragment::class.java.toString()))
             }
         }
     }
@@ -87,13 +91,39 @@ class UiTestHome {
     @Test
     fun testDrawerMenuItems() {
         activity.applyView<NavigationView>(R.id.drawerNavi) {
-            menu?.let { nonNullMenu ->
-                assertThat(nonNullMenu.findItem(R.id.action_products), CoreMatchers.`is`(notNullValue()))
-                assertThat(nonNullMenu.findItem(R.id.action_internet), CoreMatchers.`is`(notNullValue()))
-                assertThat(nonNullMenu.findItem(R.id.action_software_licenses), CoreMatchers.`is`(notNullValue()))
-                assertThat(nonNullMenu.findItem(R.id.action_about), CoreMatchers.`is`(notNullValue()))
-                assertThat(nonNullMenu.findItem(R.id.action_other).findSubItem(R.id.action_software_licenses), CoreMatchers.`is`(notNullValue()))
-                assertThat(nonNullMenu.findItem(R.id.action_other).findSubItem(R.id.action_about), CoreMatchers.`is`(notNullValue()))
+            menu.run {
+                assertThat(findItem(R.id.action_products), `is`(notNullValue()))
+                assertThat(findItem(R.id.action_internet), `is`(notNullValue()))
+                assertThat(findItem(R.id.action_software_licenses), `is`(notNullValue()))
+                assertThat(findItem(R.id.action_about), `is`(notNullValue()))
+                assertThat(findItem(R.id.action_other).findSubItem(R.id.action_software_licenses), `is`(notNullValue()))
+                assertThat(findItem(R.id.action_other).findSubItem(R.id.action_about), `is`(notNullValue()))
+            }
+        }
+    }
+
+    @Test
+
+    fun testDrawerMenuItemSelection() {
+        activity.apply {
+            applyView<NavigationView>(R.id.drawerNavi) {
+                menu.run {
+                    shadowOf(this@apply).run {
+                        performIdentifierAction(R.id.action_products, 0)
+                        assertThat(nextStartedActivity.component.className, `is`(ProductsActivity::class.java.name))
+
+                        performIdentifierAction(R.id.action_internet, 0)
+                        val startedIntent = nextStartedActivity
+                        assertThat(startedIntent.action, `is`(Intent.ACTION_VIEW))
+                        assertThat(startedIntent.dataString, `is`(getString(R.string.internet_url)))
+
+                        performIdentifierAction(R.id.action_software_licenses, 0)
+                        assertThat(nextStartedActivity.component.className, `is`(SoftwareLicensesActivity::class.java.name))
+
+                        performIdentifierAction(R.id.action_about, 0)
+                        assertThat(nextStartedActivity.component.className, `is`(AboutActivity::class.java.name))
+                    }
+                }
             }
         }
     }
