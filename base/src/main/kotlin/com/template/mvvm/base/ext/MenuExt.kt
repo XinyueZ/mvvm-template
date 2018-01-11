8b97946fc4a85ce2c531b43285efc973817793fd
Chris Zhao <xinyue.zhao@immonet.de>
2018-01-11 15:36:02 +0100

    Optimized ```NavigationView.getMenuItemView```

--- base/src/main/kotlin/com/template/mvvm/base/ext/MenuExt.kt
+++ base/src/main/kotlin/com/template/mvvm/base/ext/MenuExt.kt
@@ -12,18 +12,13 @@ fun NavigationView.getMenuItemView(@IdRes id: Int): NavigationMenuItemView? {
     for (i in 0 until childCount) {
         with(getChildAt(i)) {
             if (this is NavigationMenuView) {
-                val itemViews = (0 until childCount).map { getChildAt(it) }.filter { it is NavigationMenuItemView }
-                val menuItemsWithId = itemViews.filter {
-                    (it as NavigationMenuItemView).run {
-                        this.itemData.itemId == id
-                    }
-                }
-                if (menuItemsWithId.isNotEmpty()) {
-                    return menuItemsWithId[0] as NavigationMenuItemView
-                }
+                return (0 until childCount)
+                        .map { getChildAt(it) }
+                        .filter { it is NavigationMenuItemView }
+                        .map { it as NavigationMenuItemView }
+                        .firstOrNull { it.itemData.itemId == id }
             }
         }
     }
-
     return null
 }
