cce5e2fb6d483474775c22b5250642af0d63515d
Chris Zhao <xinyue.zhao@immonet.de>
2018-02-04 22:16:01 +0100

    Enhanced testRegisterLifecycleOwner

--- core/src/test/kotlin/com/template/mvvm/core/models/product/TestProductsViewModel.kt
+++ core/src/test/kotlin/com/template/mvvm/core/models/product/TestProductsViewModel.kt
@@ -5,22 +5,16 @@ import io.kotlintest.properties.Gen
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
-import org.mockito.junit.MockitoJUnit
 import org.hamcrest.CoreMatchers.`is` as matchIs
 import org.mockito.Mockito.`when` as mockWhen
 
 class TestProductsViewModel {
     @Mock
     private lateinit var vm: ProductsViewModel
-
-    @Rule
-    fun test() = MockitoJUnit.rule()
 
     @Before
     fun setUp() {
@@ -29,15 +23,8 @@ class TestProductsViewModel {
 
     @Test
     fun testRegisterLifecycleOwner() {
-        // Define logical
         val lifeThing = mock(LifecycleOwner::class.java)
         mockWhen(vm.lifecycleOwner).thenReturn(lifeThing)
-
-        // Complete logical
-        vm.registerLifecycleOwner(lifeThing)
-
-        // Verify result of logical
-        verify(vm).registerLifecycleOwner(lifeThing)
         assertThat(
             vm.lifecycleOwner, `is`(notNullValue())
         )
@@ -45,10 +32,21 @@ class TestProductsViewModel {
             lifeThing,
             `equalTo`(vm.lifecycleOwner)
         )
+
+        val lifeThingOther = mock(LifecycleOwner::class.java)
+        vm.registerLifecycleOwner(lifeThingOther)
+        mockWhen(vm.lifecycleOwner).thenReturn(lifeThingOther)
+        assertThat(
+            vm.lifecycleOwner, `is`(notNullValue())
+        )
+        assertThat(
+            lifeThingOther,
+            `equalTo`(vm.lifecycleOwner)
+        )
     }
 
     @Test
     fun testOnBound() {
-       mockWhen(vm.onBound(Gen.negativeIntegers().generate())).thenThrow(IndexOutOfBoundsException())
+        mockWhen(vm.onBound(Gen.negativeIntegers().generate())).thenThrow(IndexOutOfBoundsException())
     }
 }
