95f3f62692ca537543222c1e83dbee6b3c1cf013
Chris Zhao <xinyue.zhao@immonet.de>
2018-02-05 09:57:04 +0100

    I have understood Mochito

--- core/src/test/kotlin/com/template/mvvm/core/models/product/TestProductsViewModel.kt
+++ core/src/test/kotlin/com/template/mvvm/core/models/product/TestProductsViewModel.kt
@@ -1,44 +1,51 @@
 package com.template.mvvm.core.models.product
 
+import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleOwner
-import io.kotlintest.properties.Gen
+import com.template.mvvm.repository.contract.ProductsDataSource
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.*
+import org.hamcrest.Matchers.equalTo
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito.*
-import org.mockito.MockitoAnnotations
+import org.mockito.Mockito.mock
 import org.hamcrest.CoreMatchers.`is` as matchIs
 import org.mockito.Mockito.`when` as mockWhen
 
 class TestProductsViewModel {
     @Mock
+    private lateinit var dataSource: ProductsDataSource
+    @Mock
+    private lateinit var lifecycle: Lifecycle
+
     private lateinit var vm: ProductsViewModel
 
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this)
+        //MockitoAnnotations.initMocks(this)
+
+        dataSource = mock(ProductsDataSource::class.java)
+
+        lifecycle = mock(Lifecycle::class.java)
+        mockWhen(lifecycle.currentState).thenReturn(Lifecycle.State.DESTROYED)
+
+        vm = ProductsViewModel(dataSource)
     }
 
     @Test
     fun testRegisterLifecycleOwner() {
         val lifeThing = mock(LifecycleOwner::class.java)
-        mockWhen(vm.lifecycleOwner).thenReturn(lifeThing)
-        assertThat(
-            vm.lifecycleOwner, `is`(notNullValue())
-        )
+        mockWhen(lifeThing.lifecycle).thenReturn(lifecycle)
+
+        vm.registerLifecycleOwner(lifeThing)
         assertThat(
             lifeThing,
             `equalTo`(vm.lifecycleOwner)
         )
 
         val lifeThingOther = mock(LifecycleOwner::class.java)
+        mockWhen(lifeThingOther.lifecycle).thenReturn(lifecycle)
         vm.registerLifecycleOwner(lifeThingOther)
-        mockWhen(vm.lifecycleOwner).thenReturn(lifeThingOther)
-        assertThat(
-            vm.lifecycleOwner, `is`(notNullValue())
-        )
         assertThat(
             lifeThingOther,
             `equalTo`(vm.lifecycleOwner)
@@ -47,6 +54,5 @@ class TestProductsViewModel {
 
     @Test
     fun testOnBound() {
-        mockWhen(vm.onBound(Gen.negativeIntegers().generate())).thenThrow(IndexOutOfBoundsException())
     }
 }
