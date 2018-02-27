52f3d99d72166835e84b8e222820c71b79804412
Chris Zhao <xinyue.zhao@immonet.de>
2018-02-27 22:50:34 +0100

    Opotimized import

--- core/src/main/kotlin/com/template/mvvm/core/models/home/HomeViewModelState.kt
+++ core/src/main/kotlin/com/template/mvvm/core/models/home/HomeViewModelState.kt
@@ -3,7 +3,6 @@ package com.template.mvvm.core.models.home
 import android.databinding.BaseObservable
 import android.databinding.ObservableInt
 import com.template.mvvm.core.R
-import com.template.mvvm.core.arch.SingleLiveData
 
 class HomeViewModelState : BaseObservable() {
     val title = ObservableInt(R.string.home_title)
