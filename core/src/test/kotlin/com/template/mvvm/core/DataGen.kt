a7068da4ffc941b24a2094cf708eb569f408fed6
Chris Zhao <xinyue.zhao@immonet.de>
2018-03-01 10:15:54 +0100

    Added GenData for tests to generate random test domains

---
+++ core/src/test/kotlin/com/template/mvvm/core/DataGen.kt
@@ -0,0 +1,88 @@
+package com.template.mvvm.core
+
+import android.net.Uri
+import com.template.mvvm.repository.domain.licenses.Library
+import com.template.mvvm.repository.domain.licenses.License
+import com.template.mvvm.repository.domain.products.Image
+import com.template.mvvm.repository.domain.products.Product
+import com.template.mvvm.repository.domain.products.ProductDetail
+import io.kotlintest.properties.Gen
+import java.lang.StringBuilder
+
+fun generateLicenseList(size: Int) = object :
+    Gen<List<Library>> {
+    override fun generate(): List<Library> = mutableListOf<Library>().apply {
+        for (i in 0 until size) {
+            add(
+                Library(
+                    Gen.string().generate(),
+                    Gen.string().generate(),
+                    Gen.string().generate(),
+                    License(
+                        Gen.string().generate(),
+                        Gen.string().generate()
+                    )
+                )
+            )
+        }
+    }
+}
+
+fun generateProductList(size: Int) = object :
+    Gen<List<Product>> {
+    override fun generate(): List<Product> = mutableListOf<Product>().apply {
+        for (i in 0 until size) {
+            add(Product(Gen.positiveIntegers().generate().toLong()))
+        }
+    }
+}
+
+fun generateProductDetail(withPictures: Boolean = false) = object :
+    Gen<ProductDetail> {
+    override fun generate() = ProductDetail(
+        Gen.positiveIntegers().generate().toLong(),
+        Gen.string().generate(),
+        Gen.string().generate(),
+        emptyList()
+    )
+}
+
+fun generateProductImages(pid: Long) = object :
+    Gen<List<Image>> {
+    override fun generate() =
+        mutableListOf(generateProductImage(pid).generate()).apply {
+            (1 until Gen.positiveIntegers().generate()).asSequence()
+                .forEach { add(generateProductImage(pid).generate()) }
+        }
+}
+
+fun generateProductImage(pid: Long) = object :
+    Gen<Image> {
+    override fun generate() = Image(
+        pid,
+        generateProductImageSize().generate(),
+        Uri.parse(
+            StringBuilder().append("http://")
+                .append("www.")
+                .append("test")
+                .append(Gen.string().generate())
+                .append(".com")
+                .toString()
+        )
+    )
+}
+
+fun generateProductImageSize() = object :
+    Gen<String> {
+    override fun generate() = Gen.oneOf(
+        listOf(
+            "Small",
+            "XLarge",
+            "Medium",
+            "Large",
+            "Original",
+            "IPhone",
+            "IPhoneSmall"
+        )
+    ).generate()
+}
