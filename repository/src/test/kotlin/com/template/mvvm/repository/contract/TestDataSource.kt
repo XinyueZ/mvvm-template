8d2a352a8962c371f016624316e88d2e4d9a6428
Chris Zhao <xinyue.zhao@immonet.de>
2018-03-05 16:16:14 +0100

    Added tests for "select" when localOnly

--- repository/src/test/kotlin/com/template/mvvm/repository/contract/TestDataSource.kt
+++ repository/src/test/kotlin/com/template/mvvm/repository/contract/TestDataSource.kt
@@ -1,7 +1,12 @@
 package com.template.mvvm.repository.contract
 
-import kotlinx.coroutines.experimental.Job
+import io.kotlintest.properties.Gen
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.channels.consumeEach
 import kotlinx.coroutines.experimental.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,38 +18,46 @@ import org.robolectric.RobolectricTestRunner
 class TestDataSource {
     @Mock
     private lateinit var datasource: DataSource
-
-    private lateinit var testJob: Job
+    private lateinit var remoteSource: List<Int>
+    private lateinit var localSource: List<Int>
+    private lateinit var emptyT: (suspend () -> List<Int>)
+    private lateinit var predicateAcceptLocalOnly: (suspend (List<Int>) -> Boolean)
+    private lateinit var remote: (suspend () -> List<Int>?)
+    private lateinit var local: (suspend () -> List<Int>?)
+    private lateinit var saveAfterRemote: (suspend (List<Int>) -> Unit?)
 
     @Before
     fun setUp() {
         datasource = mock(DataSource::class.java)
-        testJob = Job()
+        remoteSource = Gen.list(Gen.negativeIntegers()).generate()
+        localSource = Gen.list(Gen.positiveIntegers()).generate()
+        emptyT = { emptyList() }
+        predicateAcceptLocalOnly = { it.isNotEmpty() }
+        remote = { remoteSource }
+        local = { localSource }
+        saveAfterRemote = { list -> println(list.toString()) }
     }
 
     @Test
-    fun testSelectLocalOnly() {
-        // TODO select() should be tested.
-        val remoteSource = "remote source"
-        val localSoruce = "local source"
-
-        val emptyT: (suspend () -> String) = { "" }
-        val predicateAcceptLocalOnly: (suspend (String) -> Boolean) = { it.isBlank() }
-        val remote: (suspend () -> String?) = { remoteSource }
-        val local: (suspend () -> String?) = { localSoruce }
-        val saveAfterRemote: ( suspend (String) -> Unit?) = {}
-
-        runBlocking(testJob) {
-            datasource.select(
-                    testJob,
-                    remote,
-                    saveAfterRemote,
-                    local,
-                    predicateAcceptLocalOnly,
-                    emptyT,
-                    true,
-                    {}
-                    )
+    fun testSelectLocalOnly() = runBlocking {
+        datasource.select(
+            CommonPool,
+            remote,
+            saveAfterRemote,
+            local,
+            predicateAcceptLocalOnly,
+            emptyT,
+            true,
+            {}
+        ).consumeEach {
+            assertThat(
+                this,
+                `is`(notNullValue())
+            )
+            assertThat(
+                it == localSource,
+                `is`(true)
+            )
         }
     }
 }
